895c7dda12eadf8c869e501e5c6a5931fc3c1a52
Adam Kestell <a.kestell89@hotmail.com>
2024-05-14 19:22:17 +0000

    Added Post Model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-akestell-djangoblog-hmh21bz2946.ws-eu111.gitpod.io', '.herokuapp.com']
 
@@ -86,8 +86,14 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("postgres://ulkwqat9eob:XiNMC6sflClW@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/click_shaft_petri_174301"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.gitpod.io"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
