7b4ff46b8a315a468acdb6f68b131d4afc0eeb87
Adam Kestell <a.kestell89@hotmail.com>
2024-05-26 16:21:28 +0000

    Added Authentication

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-akestell-djangoblog-hmh21bz2946.ws-eu114.gitpod.io', '.herokuapp.com']
 
